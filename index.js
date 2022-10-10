a1fd3df33cccf66a907c4f74df8c0f671743ab73
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-10-10 16:35:02 -0500

    Add JS functionality for menu open and closing

---
+++ index.js
@@ -0,0 +1,11 @@
+const mobileBtn = document.getElementById("mobile-cta");
+const nav = document.querySelector("nav");
+const mobileBtnExit = document.getElementById("mobile-exit");
+
+mobileBtn.addEventListener("click", () => {
+  nav.classList.add("menu-btn");
+});
+
+mobileBtnExit.addEventListener("click", () => {
+  nav.classList.remove("menu-btn");
+});
