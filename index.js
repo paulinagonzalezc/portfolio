df16c2f413f264a2e052c040a555e3d794b59efd
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-10-10 18:33:53 -0500

    Add menu options click functionality

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const mobileBtn = document.getElementById("mobile-cta");
-const nav = document.querySelector("nav");
-const mobileBtnExit = document.getElementById("mobile-exit");
+nav = document.querySelector("nav");
+mobileBtnExit = document.getElementById("mobile-exit");
+portfolioBtn = document.getElementById("menu-portfolio");
+aboutBtn = document.getElementById("menu-about");
+contactBtn = document.getElementById("menu-contact");
 
 mobileBtn.addEventListener("click", () => {
   nav.classList.add("menu-btn");
 });
 
 mobileBtnExit.addEventListener("click", () => {
+  nav.classList.remove("menu-btn");
+});
+
+portfolioBtn.addEventListener("click", () => {
+  nav.classList.remove("menu-btn");
+});
+
+aboutBtn.addEventListener("click", () => {
+  nav.classList.remove("menu-btn");
+});
+
+contactBtn.addEventListener("click", () => {
   nav.classList.remove("menu-btn");
 });
