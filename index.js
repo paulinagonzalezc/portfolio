722a49c877326b9d7a87584f84f8390a200d0756
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-10-10 19:28:08 -0500

    Fix eslint errors

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const mobileBtn = document.getElementById("mobile-cta");
-nav = document.querySelector("nav");
-mobileBtnExit = document.getElementById("mobile-exit");
-portfolioBtn = document.getElementById("menu-portfolio");
-aboutBtn = document.getElementById("menu-about");
-contactBtn = document.getElementById("menu-contact");
+const mobileBtn = document.getElementById('mobile-cta');
+const nav = document.querySelector('nav');
+const mobileBtnExit = document.getElementById('mobile-exit');
+const portfolioBtn = document.getElementById('menu-portfolio');
+const aboutBtn = document.getElementById('menu-about');
+const contactBtn = document.getElementById('menu-contact');
 
-mobileBtn.addEventListener("click", () => {
-  nav.classList.add("menu-btn");
+mobileBtn.addEventListener('click', () => {
+  nav.classList.add('menu-btn');
 });
 
-mobileBtnExit.addEventListener("click", () => {
-  nav.classList.remove("menu-btn");
+mobileBtnExit.addEventListener('click', () => {
+  nav.classList.remove('menu-btn');
 });
 
-portfolioBtn.addEventListener("click", () => {
-  nav.classList.remove("menu-btn");
+portfolioBtn.addEventListener('click', () => {
+  nav.classList.remove('menu-btn');
 });
 
-aboutBtn.addEventListener("click", () => {
-  nav.classList.remove("menu-btn");
+aboutBtn.addEventListener('click', () => {
+  nav.classList.remove('menu-btn');
 });
 
-contactBtn.addEventListener("click", () => {
-  nav.classList.remove("menu-btn");
+contactBtn.addEventListener('click', () => {
+  nav.classList.remove('menu-btn');
 });
